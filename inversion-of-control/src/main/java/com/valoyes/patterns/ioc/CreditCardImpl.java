3f2a3a94c490bf2e4f7d2156b5382e99fcce85a0
bavaloyes <bavaloyes@ard01.mioct.ext.sopra>
2018-04-09 18:27:09 +0200

    add @Autowired and @Component annotations

--- inversion-of-control/src/main/java/com/valoyes/patterns/ioc/CreditCardImpl.java
+++ inversion-of-control/src/main/java/com/valoyes/patterns/ioc/CreditCardImpl.java
@@ -1,5 +1,10 @@
 package com.valoyes.patterns.ioc;
 
+import org.springframework.stereotype.Component;
+
+//la anotacion component dice a Spring que en runtime esta clase puede ser creada o 
+//debe ser creada, de modo que pueda ser utilizada en otra clases
+@Component
 public class CreditCardImpl implements CreditCard{
 
 	@Override
--- inversion-of-control/src/main/java/com/valoyes/patterns/ioc/CustomerImpl.java
+++ inversion-of-control/src/main/java/com/valoyes/patterns/ioc/CustomerImpl.java
@@ -1,8 +1,19 @@
 package com.valoyes.patterns.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// la anotacion component dice a Spring que en runtime esta clase puede ser creada o 
+// debe ser creada, de modo que pueda ser utilizada en otra clases
+@Component
 public class CustomerImpl implements Customer {
 
-	CreditCard creditCard = new CreditCardImpl();
+	// al marcar un campo como @Autowired Spring buscara dicha clase y creara un objeto de la misma 
+	// y lo injectar'a, siempre y cuando la clase a inyectar este marcada con la annotation @Autowired
+	// si en este caso hacemos @Autowired de una interface, Spring es capaz de encontrar una implementacion
+	// de esta interface
+	@Autowired
+	CreditCard creditCard;
 	
 	@Override
 	public void pay() {
