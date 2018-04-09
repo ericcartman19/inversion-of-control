588eafd19ebf3c45489dc4fde24fecf229cac8f5
bavaloyes <bavaloyes@ard01.mioct.ext.sopra>
2018-04-09 18:40:44 +0200

    add TU for field @Autowiring

--- inversion-of-control/src/test/java/com/valoyes/patterns/ioc/InversionOfControlApplicationTests.java
+++ inversion-of-control/src/test/java/com/valoyes/patterns/ioc/InversionOfControlApplicationTests.java
@@ -2,6 +2,7 @@ package com.valoyes.patterns.ioc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class InversionOfControlApplicationTests {
 
+	@Autowired
+	Customer customer;
+	
 	@Test
-	public void contextLoads() {
+	public void testPayment() {
+		 customer.pay();
 	}
 
 }
